ba792d136862fa48492374e4bb14334b6c58d5dc
Seryazi-Andela <phillip.seryazi@gmail.com>
2019-08-03 14:30:41 +0300

    Feature(add): add ability to add product [Finishes #167520057]

--- sellex/apps/products/urls.py
+++ sellex/apps/products/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import HomeView
+from .views import HomeView, ProductView
 
 urlpatterns = [
     path('home/', HomeView.as_view(), name='home'),
+    path('manage/', ProductView.as_view(), name='manage_product')
 ]
--- sellex/apps/products/views.py
+++ sellex/apps/products/views.py
@@ -1,8 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import (View, )
+from .forms import AddProductForm, UpdateProductForm
+from django.contrib import auth
+import cloudinary
 
 
 # Create your views here.
 class HomeView(View):
     def get(self, request):
         return render(request, 'products/home.html', {'form': ''})
+
+
+class ProductView(View):
+    def get(self, request):
+        form = AddProductForm()
+        return render(request, 'products/add_product.html', {'form': form})
+
+    def post(self, request):
+        form = AddProductForm(request.POST, request.FILES)
+        image_url = cloudinary.uploader.upload(request.FILES['image'])['url']
+        if form.is_valid():
+            current_user = auth.get_user(request)
+            form.instance.user = current_user
+            form.instance.image = image_url
+            form.save()
+            return redirect('prods:manage_product')
+
+        form = AddProductForm()
+        return render(request, 'products/add_product.html', {'form': form})
+
+    # def put(self, request):
+    #     form = AddProductForm(request.PUT)
+    #     pass
+    #
+    # def delete(self, request):
+    #     pass
