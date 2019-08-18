fa2cf663ce75aa5823a30fb798dd0f18e9a4ff7a
Seryazi-Andela <phillip.seryazi@gmail.com>
2019-08-18 15:48:18 +0300

    Feature(payment): Add a payment portal [Finishes #167891325]

--- sellex/apps/cart/models.py
+++ sellex/apps/cart/models.py
@@ -5,3 +5,9 @@ from django.db import models
 class ShoppingCart(models.Model):
     product = models.IntegerField(default=0)
     user = models.IntegerField(default=0)
+
+
+class Order(models.Model):
+    product = models.IntegerField(default=0)
+    user = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
--- sellex/apps/cart/urls.py
+++ sellex/apps/cart/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import AddToCartView, GetCartView, RemoveFromCartView
+from .views import AddToCartView, GetCartView, RemoveFromCartView, PaymentView
 
 urlpatterns = [
     path('add/<int:id>/<int:uid>/', AddToCartView.as_view(), name='add_to_cart'),
     path('get/', GetCartView.as_view(), name='get_cart'),
     path('remove/<int:pk>/', RemoveFromCartView.as_view(), name='remove_from_cart'),
+    path('payment/', PaymentView.as_view(), name='payment'),
 ]
